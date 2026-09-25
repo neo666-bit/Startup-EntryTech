ac2c22fcec2a4b43d8a2d4fd0d939a1e0606da86
Julio <marciano.julinho@gmail.com>
2026-09-25 11:17:40 -0300

    Git Inicial

---
+++ src/app/landing-page/landing-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-landing-page',
+  styleUrl: './landing-page.css',
+  templateUrl: './landing-page.html',
+})
+export class LandingPage {}
